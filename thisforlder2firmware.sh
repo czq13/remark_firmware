1d36f72d72c4ec678dd43a469ce1caf99163cbc0
czq <czq92@qq.com>
2020-05-01 11:19:28 +0800

    add thisforlder2firmware

---
+++ thisforlder2firmware.sh
@@ -0,0 +1,16 @@
+yesththisfolder=/home/czq/remark_firmware
+pathfirmware=/home/czq/new_version/Firmware
+cp $paththisfolder/rc.vtol_defaults $pathfirmware/ROMFS/px4fmu_common/init.d/
+cp $paththisfolder/firefly6.aux.mix $pathfirmware/ROMFS/px4fmu_common/mixers/
+cp $paththisfolder/firefly6.main.mix $pathfirmware/ROMFS/px4fmu_common/mixers/
+cp $paththisfolder/CMakeLists.txt $pathfirmware/msg/
+cp $paththisfolder/drv_pwm_output.h $pathfirmware/src/drivers/
+cp $paththisfolder/mavlink_main.cpp $pathfirmware/src/modules/mavlink/
+cp $paththisfolder/mavlink_messages.cpp $pathfirmware/src/modules/mavlink/
+cp $paththisfolder/mavlink_receiver.cpp $pathfirmware/src/modules/mavlink/
+cp $paththisfolder/mavlink_receiver.h $pathfirmware/src/modules/mavlink/
+cp $paththisfolder/tiltrotor.cpp $pathfirmware/src/modules/vtol_att_control/
+cp $paththisfolder/tiltrotor.h $pathfirmware/src/modules/vtol_att_control/
+cp $paththisfolder/ch_actuator_controls.msg $pathfirmware/msg/
+cp $paththisfolder/ch_actuator_state.msg $pathfirmware/msg/
+
